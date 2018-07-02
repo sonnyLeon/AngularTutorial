482365f8c33853f10b338050753df096cfb87ea8
sonnyLeon <sonnyLeon@users.noreply.github.com>
2018-07-02 21:22:51 +1000

    Add countdown, interval and log
    This add the interval and log

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 (function() {
   var app = angular.module("gitHubViewer", []);
 
-  var MainController = function($scope, $http) {
+  var MainController = function($scope, $http, $interval, $log) {
 
     var onUserComplete = function(response) {
       $scope.user = response.data;
@@ -17,18 +17,38 @@
     var onError = function(reason) {
       $scope.error = "Couldn't fetch the user";
     };
+    
+    var decrementCountdown = function() {
+      $scope.countdown -= 1;
+      if($scope.countdown < 1){
+        $scope.search($scope.username);
+      }
+    };
+    
+    var countdownInterval = null;
+    var startCountdown = function() {
+      countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+    };
 
     $scope.search = function(username) {
+      $log.info("searching for " + username);
       $http.get("https://api.github.com/users/" + username)
         .then(onUserComplete, onError);
+        
+      if(countdownInterval) {
+        $interval.cancel(countdownInterval);
+        $scope.countdown = null;
+      }
     }
 
     $scope.message = "GitHub Viewer";
     $scope.username = "angular";
     $scope.repoSortOrder = "-stargazers_count"
+    $scope.countdown = 5;
+    startCountdown();
 
   };
 
-  app.controller("MainController", ["$scope", "$http", MainController]);
+  app.controller("MainController", ["$scope", "$http", "$interval", "$log", MainController]);
 
 }());
